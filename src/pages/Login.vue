92845fb81360ea8a7a0eecb3a1bc1ab3408707ec
ishihara <r.funk.7188@gmail.com>
2020-03-10 09:44:46 +0900

    ログイン画面作成

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,26 @@
 <template>
-  <h2>login page</h2>
+  <div>
+    <div class="jumbotron bg-danger text-white">
+      <h2>#SLACK#</h2>
+      <p>チャットをはじめよう</p>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col text-center">
+          <button class="btn btn-outline-danger btn-lg">
+            Googleでログイン
+          </button>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col text-center">
+          <button class="btn btn-outline-info btn-lg">
+            Twitterでログイン
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +28,8 @@ export default {
   name: 'login',
 }
 </script>
+
+<style scoped>
+.btn {
+}
+</style>
