342c26deb47b2688a265baedefb0afe444910f9c
ishihara <r.funk.7188@gmail.com>
2020-03-10 09:32:20 +0900

    ページを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Chat from '@/pages/Chat'
+import Login from '@/pages/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      name: 'chat',
+      component: Chat,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+  ],
 })
