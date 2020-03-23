c8f02655f5d55611b117d7a5ae88204350066fb6
ishihara <r.funk.7188@gmail.com>
2020-03-23 10:40:32 +0900

    画像アップロード機能の実装

--- src/messages/MessageForm.vue
+++ src/messages/MessageForm.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <div class="messageform">
+      <div v-if="uploadState !== null" class="progress">
+        <div
+          class="progress-bar bg-danger progress-bar-animated"
+          role="progressbar"
+        >
+          {{ uploadLabel }}
+        </div>
+      </div>
+
       <form @submit.prevent="sendMessage">
         <div class="input-group mb-3">
           <input
@@ -21,31 +30,63 @@
             </button>
           </div>
           <div class="input-group-append">
-            <button class="btn btn-dark mt-3" type="button">
+            <button
+              @click.prevent="openFileModal"
+              class="btn btn-dark mt-3"
+              type="button"
+            >
               &nbsp; 添付 &nbsp;
             </button>
           </div>
         </div>
       </form>
+
+      <file-modal ref='file_modal'></file-modal>
     </div>
   </div>
 </template>
 
 <script>
+import FileModal from './FileModal'
+import { v4 as uuidv4 } from 'uuid'
 import { mapGetters } from 'vuex'
+import storage from 'firebase/storage'
 
 export default {
   name: 'message-form',
+
+  components: {
+    FileModal,
+  },
 
   data() {
     return {
       message: '',
       errors: [],
+      storageRef: firebase.storage().ref(),
+      uploadTask: null,
+      uploadState: null,
     }
   },
 
   computed: {
-    ...mapGetters(['currentChannel', 'currentUser']),
+    ...mapGetters(['currentChannel', 'currentUser', 'isPrivate']),
+
+    uploadLabel() {
+      switch (this.uploadState) {
+        case 'uploading':
+          return '送信中'
+          break
+        case 'error':
+          return 'エラー'
+          break
+        case 'done':
+          return '送信完了'
+          break
+        default:
+          return ''
+      }
+    },
   },
 
   methods: {
@@ -63,7 +104,8 @@ export default {
       if (this.currentChannel !== null) {
         if (this.message.length > 0) {
           console.log(this.$parent)
-          this.$parent.getMsgRef()
+          this.$parent
+            .getMsgRef()
             .child(this.currentChannel.id)
             .push()
             .set(newMessage)
@@ -80,6 +122,56 @@ export default {
         }
       }
     },
+
+    uploadFile(file, metadata) {
+      if (file === null) return false
+
+      let pathToUpload = this.currentChannel.id
+      let ref = this.$parent.getMsgRef()
+
+      let filePath = this.getPath() + '/' + uuidv4() + '.jpg'
+
+      this.uploadTask = this.storageRef.child(filePath).put(file, metadata)
+      this.uploadState = 'uploading'
+
+      this.uploadTask.on(
+        'state_changed',
+        snapshot => {
+          // progress
+          let percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          $('.progress-bar').css('width', percent + '%')
+        },
+        error => {
+          // errors
+          this, errors.push(error.message)
+          this.uploadState = 'error'
+          this.uploadTask = null
+        },
+        () => {
+          // finished
+          this.uploadState = 'done'
+          this.$refs.file_modal.resetForm()
+        }
+      )
+    },
+
+    getPath() {
+      if (this.isPrivate) {
+        return 'chat/private/' + this.currentChannel.id
+      } else {
+        return 'chat/public'
+      }
+    },
+
+    openFileModal() {
+      $('#fileModal')
+        .appendTo('body')
+        .modal('show')
+    },
+  },
+
+  mounted() {
+    $('html, body').scrollTop($(document).height())
   },
 }
 </script>
@@ -99,5 +191,8 @@ export default {
 input,
 button {
   height: 50px;
+}
+.progress {
+  margin-bottom: -16px;
 }
 </style>
